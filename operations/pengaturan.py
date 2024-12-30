012dfcee0043540836f53674d6dbb45b5c62eb33
Yudananto OS <yudanantoos@gmail.com>
2024-12-30 17:06:21 +0700

    hapus fungsi simpan_pengaturan

--- operations/pengaturan.py
+++ operations/pengaturan.py
@@ -2,15 +2,17 @@ import configparser
 import os
 
 pengaturan = configparser.ConfigParser()
+"""
 pengaturan['DEFAULT'] = {'DataDir' : '../Data',
-                         'DataFile' : '%(datadir)s/data-overtime.json',
+                         'DataFile' : '%(DataDir)s/data-overtime.json',
                          'Gapok' : 0}
 
 def simpan_pengaturan():
     with open('config.ini', 'w') as simpan:
         pengaturan.write(simpan)
+"""
 
 def load_pengaturan():
-    pengaturan.read('tes.ini')
+    pengaturan.read('config.ini')
     pengaturan.sections()
     return pengaturan
