c1298b4f5f54049d348e85ea3be61c94f38bf6e1
Yudananto OS <yudanantoos@gmail.com>
2025-01-08 17:09:51 +0700

    save & tes class

---
+++ other/other_overtime.py
@@ -0,0 +1,8 @@
+class Overtime:
+
+    jam_lembur = 0
+    rumus = 1/173
+    gapok = 0
+
+    def __init__(self, hari):
+        self.hari = hari
--- tester.py
+++ tester.py
@@ -1,7 +1,27 @@
-class Tes(object):
+class Map:
+    def __init__(self, iterate):
+        self.list = []
+        self.__geek(iterate)
 
-    def __init__(self, nama):
-        self.nama = []
+    def geek(self, iterate):
+        for item in iterate:
+            self.list.append(item)
 
-    def kumpulan(self):
-        return self.nama
+    # private copy of original geek() method
+    __geek = geek
+
+
+class MapSubclass(Map):
+
+    # provides new signature for geek() but
+    # does not break __init__()
+    def geek(self, key, value):
+        for i in zip(key, value):
+            self.list.append(i)
+
+m = Map("Tes")
+print(m.list)
+
+tes = {'ok': 'ok banget'}
+ms = MapSubclass(tes.keys())
+print(ms.list)
