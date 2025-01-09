d392d007c7e951a983f3280cd7148ec4ea0f57a5
yudanantoos <yudanantoos@gmail.com>
2025-01-09 07:10:38 +0700

    just save

--- other/other_overtime.py
+++ other/other_overtime.py
@@ -1,8 +1,5 @@
 class Overtime:
 
-    jam_lembur = 0
-    rumus = 1/173
-    gapok = 0
-
     def __init__(self, hari):
-        self.hari = hari
+        self.gapok = hari
+
--- tester.py
+++ tester.py
@@ -1,27 +1,5 @@
-class Map:
-    def __init__(self, iterate):
-        self.list = []
-        self.__geek(iterate)
+def tes(nomor):
+    if nomor % nomor == 1:
+        return tes(nomor)
 
-    def geek(self, iterate):
-        for item in iterate:
-            self.list.append(item)
-
-    # private copy of original geek() method
-    __geek = geek
-
-
-class MapSubclass(Map):
-
-    # provides new signature for geek() but
-    # does not break __init__()
-    def geek(self, key, value):
-        for i in zip(key, value):
-            self.list.append(i)
-
-m = Map("Tes")
-print(m.list)
-
-tes = {'ok': 'ok banget'}
-ms = MapSubclass(tes.keys())
-print(ms.list)
+print(tes(4))
