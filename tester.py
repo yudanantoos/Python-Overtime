09eabd13fdd941938667a39172e88237831d835e
Yudananto OS <yudanantoos@gmail.com>
2025-01-09 19:51:46 +0700

    save

--- tester.py
+++ tester.py
@@ -1,5 +1,8 @@
 def tes(nomor):
-    if nomor % nomor == 1:
-        return tes(nomor)
+    def tes_anak(nomor):
+        if nomor >= 100:
+            return 100
+        return nomor * tes(nomor + 1)
+    return tes_anak(nomor)
 
-print(tes(4))
+print(tes(1))
